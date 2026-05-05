941c8612e10e22a983eed0376b65c8757a19ba7f
Shashank Kumar <shashankmuz3@gmail.com>
2026-05-05 17:34:47 +0530

    Socket Implementation

--- server/routes/messageRouter.js
+++ server/routes/messageRouter.js
@@ -1,6 +1,6 @@
 import express from "express";
-import fetchMessage from "../controllers/message/fetchMessage.js";
-import sendMessage from "../controllers/message/sendMessage.js";
+import fetchMessage from "../controllers/Message/fetchMessage.js";
+import sendMessage from "../controllers/Message/sendMessage.js";
 import authMiddleware from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
